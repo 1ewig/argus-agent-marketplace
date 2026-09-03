88d9e5a2b6f38aff8e306791e5dff1dcd0370fa2
Asad <asadshahid234@gmail.com>
2026-09-03 15:42:02 +0500

    feat(config): add ngrok domain to allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["harmonics-paternity-blighted.ngrok-free.dev"],
 };
 
 export default nextConfig;
